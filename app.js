983f18060ebc74171b832c931d85fba71c369e98
ahlaan <mohamedahlan58@gmail.com>
2025-03-01 20:32:01 +0530

    connections to the mongodb (done)

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const addMovie = require("./controllers/addMovies");
 
 const app = experss();
 
+const mongoose = require("mongoose");
+
+//connection to mongodb
+
+mongoose
+  .connect(
+    "mongodb+srv://loonslab:loonslab@moviedb.7jrmv.mongodb.net/?retryWrites=true&w=majority&appName=MovieDB",
+    {}
+  )
+  .then(() => {
+    console.log("Connected to mongodb database");
+  })
+  .catch(() => {
+    console.log("Connection to mongodb failed");
+  });
+
 //routes
 
 app.post("/api/movies", addMovie);
