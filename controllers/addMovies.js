172c518189958eb9fd7e67571794945c9af78f46
ahlaan <mohamedahlan58@gmail.com>
2025-03-04 01:22:22 +0530

    using try catch to handle error

--- controllers/addMovies.js
+++ controllers/addMovies.js
@@ -4,17 +4,32 @@ const addMovie = (req, res) => {
   const { movie_name, info, rating } = req.body;
 
   //validations...
-  if (!movie_name || !info || !rating) {
-    return res.status(400).json({
-      status: "failed",
-      message: "Please provide all the required fields",
-    });
-  }
 
-  if (rating > 10 || rating < 0) {
+  // if (!movie_name || !info || !rating) {
+  //   return res.status(400).json({
+  //     status: "failed",
+  //     message: "Please provide all the required fields",
+  //   });
+  // }
+
+  // if (rating > 10 || rating < 0) {
+  //   return res.status(400).json({
+  //     status: "failed",
+  //     message: "Rating should be between 0 and 10",
+  //   });
+  // }
+
+  // using try catch block to handle the error & exceptions
+
+  try {
+    if (!movie_name) throw "Movie name is required";
+    if (!info) throw "Movie info is required";
+    if (!rating) throw "Movie rating is required";
+    if (rating > 10 || rating < 0) throw "Rating should be between 0 and 10";
+  } catch (err) {
     return res.status(400).json({
       status: "failed",
-      message: "Rating should be between 0 and 10",
+      message: err,
     });
   }
 
