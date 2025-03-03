9fa4018948db63706b348aed5ca6b9a0c25853b4
ahlaan <mohamedahlan58@gmail.com>
2025-03-04 01:10:01 +0530

    validating requests

--- controllers/addMovies.js
+++ controllers/addMovies.js
@@ -4,6 +4,19 @@ const addMovie = (req, res) => {
   const { movie_name, info, rating } = req.body;
 
   //validations...
+  if (!movie_name || !info || !rating) {
+    return res.status(400).json({
+      status: "failed",
+      message: "Please provide all the required fields",
+    });
+  }
+
+  if (rating > 10 || rating < 0) {
+    return res.status(400).json({
+      status: "failed",
+      message: "Rating should be between 0 and 10",
+    });
+  }
 
   //success response
   res.status(200).json({
